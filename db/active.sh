1a24f50c124b3f7008773b3737530349a5f44f4a
Ryval Samuel <ryvalonibala026@student.unsrat.ac.id>
2023-11-27 11:17:57 +0800

    added script to activated & deactivated db container

---
+++ db/active.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+## activated the postgres & pgadmin
+docker start postgres_c pgadmin4_c
---
+++ db/deactive.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+## activated the postgres & pgadmin
+docker stop postgres_c pgadmin4_c
